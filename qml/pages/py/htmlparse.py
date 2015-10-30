1f09e4c3df49e6d809e0eaf8ce92d3dac4758403
0312birdzhang <0312birdzhang@gmail.com>
2015-10-30 17:26:14 +0800

    完成所有显示元素的爬取

--- qml/pages/py/htmlparse.py
+++ qml/pages/py/htmlparse.py
@@ -26,10 +26,18 @@ def queryStatus(url):
     articulo_autor = soup.find_all("p","articulo-autor")[0].contents[0].strip() #文章作者
     articulo_contenido = soup.find_all("div","articulo-contenido")[0].contents[0] #文章内容
 
-    #cuestion_q_icono = soup.find_all("div","cuestion-q-icono")[0].contents[0].strip() #问题-标题
-    cuestion_contenido = soup.find_all("div","cuestion-contenido")[0].contents[0].strip() #问题-问
-    cuestion_contenians = soup.find_all("div","cuestion-contenido")[0].contents[0] #问题-答
-    print cuestion_contenido,"----",cuestion_contenians
+    cuestion_title = soup.find_all("h4")[0].contents[0].strip() #问题-标题
+    cuestion_contenido = soup.find_all("div","cuestion-contenido")#问题-问
+    cuestion_question = cuestion_contenido[0].contents[0].strip() #问题-问
+    cuestion_answerer = soup.find_all("h4")[1].contents[0].strip() #问题-回答者
+    cuestion_contenians = cuestion_contenido[1].contents #问题-答
+    cuestion_contenians = u''.join(unicode(item) for item in cuestion_contenians)
+
+    #print cuestion_contenians
+
+    cosas_imagen = soup.find_all("div", "cosas-imagen")[0].find("img")['src'].strip() #东西里面的图片链接
+    cosas_titulo = soup.find_all("h2","cosas-titulo")[0].contents[0].strip() #东西名
+    cosas_contenido = soup.find_all("div","cosas-contenido")[0].contents[0].strip() #东西说明
 
 if __name__ == "__main__":
     queryStatus('http://wufazhuce.com/one/vol.1118')
